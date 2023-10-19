dd085b334df7f7325036152b85f392ec7404f512
NAB42 <fj45cruza@gmail.com>
2023-10-18 22:27:09 -0600

    Modified bootsect & added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# cronumOS
+Currently this is nothing.
+It is a goal of mine to create a functional operating system, and this is my start.
+
+Currently all it has is a non-functional bootsector.
--- bootsect.asm
+++ bootsect.asm
@@ -1,8 +1,24 @@
-; 
+; At some point this will be a bootsector, but for not it's a load of nothing.
 ;[org 0x7c00]
 ;
 mov ah, 0x0e
-mov al, 'F'
+mov al, 'c'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'm'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, '!'
 int 0x10
 
 jmp $
